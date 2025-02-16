db6ccea1a714886ef3f62360ac636612dd5afbac
Shreya Mankar <shreyamankar13@gmail.com>
2025-02-16 23:00:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shreya",
+    author_email="shreyamankar13@gmail.com",
+    packages=find_packages()
+)
